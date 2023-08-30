9c9bae2cf93e0e2c50fe7061ce41c6306196f647
Carlos-Uribe <juanuribe1920@gmail.com>
2023-08-30 01:59:50 -0500

    project

---
+++ ListaContactos.js
@@ -0,0 +1,87 @@
+let contactList = [
+  {
+    id: 1,
+    nombre: "JuanC",
+    apellido: "Uribe",
+    edad: "29",
+    celular: "3012310955",
+    ubicacion: {
+      ciudad: "Medellin",
+      direccion: "Carrera 49 Acevedo",
+    },
+  },
+  {
+    id: 2,
+    nombre: "Angelis",
+    apellido: "Gutierrez",
+    edad: "19",
+    celular: "3105454332",
+    ubicacion: {
+      ciudad: "Taraza",
+      direccion: "Carrera 33 Pavas",
+    },
+  },
+];
+
+function agregarContacto(contactoNuevo) {
+  contactList.push(contactoNuevo);
+}
+
+function eliminarContacto(contactoAEliminar) {
+  let nuevaLista = contactList.filter(
+    (contacto) => contacto.id !== contactoAEliminar
+  );
+  contactList = nuevaLista;
+}
+
+function actualizarContacto(contactoAActualizar) {
+  let indice = contactList.findIndex(
+    (contacto) => contacto.id === contactoAActualizar.id
+  );
+
+  if (indice !== -1) {
+    contactList[indice].nombre = contactoAActualizar.nombre;
+    contactList[indice].apellido = contactoAActualizar.apellido;
+    contactList[indice].ubicacion = contactoAActualizar.ubicacion;
+  }
+}
+
+function imprimirListaDeContactos() {
+  console.table(contactList); // Utilizamos console.table() para imprimir la tabla
+}
+
+let contactoNuevo = {
+  id: 3,
+  nombre: "Karen",
+  apellido: "Rodriguez",
+  edad: "33",
+  celular: "3154920404",
+  ubicacion: {
+    ciudad: "Bogota",
+    direccion: "Parque centro int 290",
+  },
+};
+
+let contactoActualizado = {
+  id: 1,
+  nombre: "Alejandro",
+  apellido: "Magno",
+  edad: "100",
+  celular: "Na",
+  ubicacion: {
+    ciudad: "Bucaramanga",
+    direccion: "Calle 29 los alpes",
+  },
+};
+
+console.log(contactoNuevo.nombre);
+
+eliminarContacto();
+
+//agregarContacto(contactoNuevo);
+
+//imprimirListaDeContactos();
+
+//actualizarContacto(contactoActualizado);
+
+console.log(contactList);
